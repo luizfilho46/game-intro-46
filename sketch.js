9411971d3eb23aae72e52d445db5f63804fad10a
luiz filho <luiz.filho@mutantbr.com>
2020-06-23 20:15:59 -0300

    pulo para o personagem e colisão

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let imagemPersonagem;
 let imagemInimigo;
 let cenario;
 let somDoJogo;
+let somDoPulo;
 let personagem;
 let inimigo;
 
@@ -37,28 +38,62 @@ const matrizInimigo = [
   [312, 626],
 ];
 
+this.matrizPersonagem = [
+  [0, 0],
+  [220, 0],
+  [440, 0],
+  [660, 0],
+  [0, 270],
+  [220, 270],
+  [440, 270],
+  [660, 270],
+  [0, 540],
+  [220, 540],
+  [440, 540],
+  [660, 540],
+  [0, 0],
+  [220, 810],
+  [440, 810],
+  [660, 810],
+]
+
 function preload() {
   imagemCenario = loadImage('imagens/cenario/floresta.png');
   imagemPersonagem = loadImage('imagens/personagem/correndo.png');
   imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
   somDoJogo = loadSound('sons/trilha_jogo.mp3');
+  somDoPulo = loadSound('sons/somPulo.mp3');
 }
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 5);
-  personagem = new Personagem(imagemPersonagem);
-  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 52, 52, 104, 104)
+  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 110, 135, 220, 270);
+  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 52, 52, 104, 104);
   frameRate(40);
   // somDoJogo.play();
   // somDoJogo.loop();
+}
+
+function keyPressed() {
+  if (key === 'ArrowUp') {
+    personagem.pular();
+    somDoPulo.play()
+  }
+
 }
 
 function draw() {
   cenario.exibe();
   cenario.mover();
   personagem.exibir();
+  personagem.aplicarGravidade()
   inimigo.exibir();
   inimigo.mover();
+
+  if (personagem.estaColidindo(inimigo)) {
+    console.log('Colidiu')
+    noLoop()
+  }
 }
